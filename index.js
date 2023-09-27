481ca758e357867292a75b965820155f266d7c12
Vitor Ramos Pacheco <40059_vitor@callink.com.br>
2023-09-27 12:53:20 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const apiKey = '6c574c1bcdc548d08f9174941231909'
+
+const searchBtn = document.querySelector('searchBtn')
+
+function getLocale() {
+    const locale = document.getElementById('locale').value
+    console.log(locale)
+    const apiUrl = 'http://api.weatherapi.com/v1/current.json?key=' + apiKey + '&q=' + locale + '&aqi=no'
+    console.log(apiUrl)
+
+    return apiUrl
+}
+
+
+async function busca(apiUrl) {
+
+    const Busca = await fetch(apiUrl).then((res) => res.json())
+    const data = Busca.current
+    const temp = (data.temp_c)
+    const city = (Busca.location.name) + ", " + (Busca.location.region)
+    const country = (Busca.location.country)
+    const humidity = (data.humidity)
+    const windSpeed = (data.wind_kph)
+    console.log(temp, humidity)
+    document.getElementById("showTemp").innerHTML = temp + "°C"
+    document.getElementById("fullCity").innerHTML = city
+    document.getElementById("showHumidity").innerHTML = humidity + "%" + `<p class="humidity">Humidity</p>`
+    document.getElementById("showWind").innerHTML = windSpeed + " Km/h" + `<p class="wind">Wind Speed</p>`
+    document.getElementById("country").innerHTML = country
+}
+
